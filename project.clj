c02293a45383bf11b9ed973866d83357c663c7a5
Michael Rubanov <mrubanov@gmail.com>
2015-01-19 20:58:34 +0200

    marginalia based docs

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "Hooke your functions!"
   :url "https://github.com/technomancy/robert-hooke"
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.4.0"]]}}
+  :plugins [[lein-marginalia "0.8.0"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"})
